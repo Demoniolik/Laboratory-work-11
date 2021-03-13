cdca54bddd92b490b09736105157cc99ee032712
dmytri <dimabekker863@gmail.com>
2021-03-13 16:20:10 +0200

    New features

--- src/main/java/com/lab11/Main.java
+++ src/main/java/com/lab11/Main.java
@@ -8,10 +8,16 @@ import org.xml.sax.SAXException;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.TransformerException;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        SAXController saxController = new SAXController("group.xml");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter input file name: ");
+        String inputFile = scanner.nextLine();
+        System.out.print("Enter output file name: ");
+        String outputFile = scanner.nextLine();
+        SAXController saxController = new SAXController(inputFile);
         try {
             saxController.parse();
         } catch (SAXException | IOException | ParserConfigurationException e) {
@@ -21,7 +27,7 @@ public class Main {
         Verifier.changeAverageScoreIfIncorrect(saxController.getGroup());
 
         try {
-            DOMController.saveToXML(saxController.getGroup(), "output.xml");
+            DOMController.saveToXML(saxController.getGroup(), outputFile);
         } catch (ParserConfigurationException | TransformerException e) {
             e.printStackTrace();
         }
